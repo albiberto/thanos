ec8e2943a8b901f0dc2171709c058adfcef9f494
Alberto Viezzi <aviezzi@outlook.com>
2025-07-06 14:10:21 +0200

    Add Support.BuildBoard helper and GetValidMoves benchmarks; refactor MonteCarlo for preallocated move arrays
    - Extract board setup logic to Support.BuildBoard for reuse in benchmarks
    - Add GetValidMovesBenchmarks for performance testing of valid move calculation
    - Refactor MonteCarlo.GetValidMoves to use preallocated arrays and return count of valid moves
    - Update Program.cs to run new benchmarks and demonstrate GetValidMoves usage

--- Thanos.Console/Program.cs
+++ Thanos.Console/Program.cs
@@ -1,4 +1,13 @@
 ﻿using BenchmarkDotNet.Running;
+using Thanos;
 using Thanos.Console;
 
 BenchmarkRunner.Run<BuildCollisionMatrixBenchmarks>();
+BenchmarkRunner.Run<GetValidMovesBenchmarks>();
+
+return;
+
+var board = Support.BuildBoard();
+var mySnake = board.snakes[0];
+
+MonteCarlo.GetValidMoves(board.width, board.height, mySnake.id, mySnake.body, mySnake.body.Length, mySnake.head.x, mySnake.head.y, board.hazards, board.hazards.Length, board.snakes, board.snakes.Length, mySnake.health < 100);
