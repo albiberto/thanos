5956b5ed1164f5858d4bc25d80f39003c16c6aac
Alberto Viezzi <aviezzi@outlook.com>
2025-07-06 15:29:53 +0200

    Optimize MonteCarlo initial exploration with aggressive task and aggregation unrolling; add GetBestMoveAsync benchmark

--- Thanos.Console/Program.cs
+++ Thanos.Console/Program.cs
@@ -1,9 +1,11 @@
 ﻿using BenchmarkDotNet.Running;
 using Thanos;
 using Thanos.Console;
+using Thanos.Model;
 
 BenchmarkRunner.Run<BuildCollisionMatrixBenchmarks>();
 BenchmarkRunner.Run<GetValidMovesBenchmarks>();
+BenchmarkRunner.Run<GetBestMoveAsync>();
 
 return;
 
@@ -11,3 +13,11 @@ var board = Support.BuildBoard();
 var mySnake = board.snakes[0];
 
 MonteCarlo.GetValidMoves(board.width, board.height, mySnake.id, mySnake.body, mySnake.body.Length, mySnake.head.x, mySnake.head.y, board.hazards, board.hazards.Length, board.snakes, board.snakes.Length, mySnake.health < 100);
+
+board.Print();
+var monteCarlo = new MonteCarlo([]);
+await monteCarlo.GetBestMoveAsync(new MoveRequest
+{
+    Board = board,
+    You = mySnake
+});
