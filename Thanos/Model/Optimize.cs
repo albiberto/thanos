5290abef36e0510666060898308178f95cd46ad4
Alberto Viezzi <aviezzi@outlook.com>
2025-07-17 00:35:12 +0200

    Add ultra-fast move endpoints and optimized request models: implement pooled buffer parsing, manual JSON deserialization, and index-based board representation; add HTTP test scripts and refactor collision matrix clearing

---
+++ Thanos/Model/Optimize.cs
@@ -0,0 +1,21 @@
+﻿namespace Thanos.Model;
+
+public class SnakeOptimized
+{
+    public string Id { get; set; } = "";
+    public int Health { get; set; }
+    public int[] BodyIndices { get; set; } = []; // <-- Array di indici!
+    public int HeadIndex { get; set; }           // <-- Indice singolo!
+    public int Length { get; set; }
+}
+
+public class MoveRequestOptimized
+{
+    public int Turn { get; set; }
+    public int BoardWidth { get; set; }
+    public int BoardHeight { get; set; }
+    public int[] FoodIndices { get; set; } = [];
+    public int[] HazardIndices { get; set; } = [];
+    public SnakeOptimized[] Snakes { get; set; } = [];
+    public SnakeOptimized You { get; set; } = new();
+}
