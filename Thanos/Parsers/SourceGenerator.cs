1a17fe33944201484e00bcac08497056efbd89e7
Alberto Viezzi <aviezzi@outlook.com>
2025-08-09 15:35:19 +0200

    Add SourceGenerator for JSON serialization of MoveRequest; implement JsonSourceGenerationOptions and define AppJsonSerializerContext

---
+++ Thanos/Parsers/SourceGenerator.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Thanos.Parsers;
+
+[JsonSourceGenerationOptions(JsonSerializerDefaults.Web)]
+[JsonSerializable(typeof(MoveRequest))]
+public partial class AppJsonSerializerContext : JsonSerializerContext;
