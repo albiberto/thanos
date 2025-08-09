eb10f79d9db9a9d28a5dd3a5c79c42ddad019857
Alberto Viezzi <aviezzi@outlook.com>
2025-08-09 17:02:49 +0200

    Refactor Game struct to use primary constructor syntax and initialize properties directly

--- Thanos/SourceGen/Game.cs
+++ Thanos/SourceGen/Game.cs
@@ -1,40 +1,23 @@
 ﻿using System.Text.Json.Serialization;
 
-// Un namespace dedicato per i modelli usati dal Source Generator
 namespace Thanos.SourceGen;
 
-// NOTA: I modelli del JSON completo (es. MoveRequest) andrebbero qui
-// public readonly struct MoveRequest(...) { ... }
-
-public readonly struct Game
+public readonly struct Game(Guid id, Ruleset ruleset, string map, string source, int timeout)
 {
-    // L'attributo [JsonPropertyName] mappa la proprietà C# (PascalCase)
-    // alla chiave JSON (camelCase)
     [JsonPropertyName("id")]
-    public Guid Id { get; }
+    public Guid Id { get; } = id;
 
     [JsonPropertyName("ruleset")]
-    public Ruleset Ruleset { get; }
+    public Ruleset Ruleset { get; } = ruleset;
 
     [JsonPropertyName("map")]
-    public string Map { get; }
+    public string Map { get; } = map;
 
     [JsonPropertyName("source")]
-    public string Source { get; }
+    public string Source { get; } = source;
 
     [JsonPropertyName("timeout")]
-    public int Timeout { get; }
-
-    // Il Source Generator è abbastanza intelligente da trovare il costruttore pubblico
-    // senza bisogno dell'attributo [JsonConstructor]
-    public Game(Guid id, Ruleset ruleset, string map, string source, int timeout)
-    {
-        Id = id;
-        Ruleset = ruleset;
-        Map = map;
-        Source = source;
-        Timeout = timeout;
-    }
+    public int Timeout { get; } = timeout;
 }
 
 public readonly struct Ruleset(string name, string version, RulesetSettings settings)
