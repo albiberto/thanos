7eaa76189166de6de0ffec42a90f72a7f90e37e7
Alberto Viezzi <alberto.viezzi@euris.it>
2025-08-09 13:50:58 +0200

    Add GameParserTest and refactor LowLevelParser for improved JSON parsing

--- Thanos/GameSerializer.cs
+++ Thanos/GameSerializer.cs
@@ -1,47 +1,48 @@
 ﻿using System.Text;
 using System.Text.Json;
-using Thanos;
 using Thanos.Parsers;
 
-// Riutilizziamo le stesse struct/classi per contenere i dati
-public struct Point { public int X { get; set; } public int Y { get; set; } }
-public struct SnakeCustomizations { public string Color { get; set; } public string Head { get; set; } public string Tail { get; set; } }
-public class Snake { public string Id { get; set; } public string Name { get; set; } public int Health { get; set; } public List<Point> Body { get; set; } public string Latency { get; set; } public Point Head { get; set; } public int Length { get; set; } public string Shout { get; set; } public SnakeCustomizations Customizations { get; set; } }
+namespace Thanos;
 
-public class LowLevelParser
+public static class LowLevelParser
 {
-    public static void Main(string[] args)
+    #region Pre-compiled UTF-8 Property Names
+    
+    private static ReadOnlySpan<byte> GameProp => "game"u8;
+    private static ReadOnlySpan<byte> TurnProp => "turn"u8;
+    
+    #endregion
+    
+    public static Game Parse(string json)
     {
-        const string jsonString = "";
+        Game game = default;
+        int turn = default;
 
-        int turn;
-
-        var jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+        var jsonBytes = Encoding.UTF8.GetBytes(json);
         var reader = new Utf8JsonReader(jsonBytes);
 
-        // Iniziamo il singolo passaggio attraverso il JSON
         while (reader.Read())
         {
             if (reader.TokenType == JsonTokenType.PropertyName)
             {
-                var propertyName = reader.GetString();
-                reader.Read(); // Avanza al valore della proprietà
-
-                switch (propertyName)
+                if (reader.ValueTextEquals(GameProp))
                 {
-                    case "game":
-                        GameParser.Parse(ref reader);
-                        break;
-                    case "turn":
-                        turn = reader.GetU();
-                        break;
-                    default:
-                        reader.Skip();
-                        break;
+                    reader.Read(); // vai al valore
+                    game = GameParser.Parse(ref reader);
+                }
+                else if (reader.ValueTextEquals(TurnProp))
+                {
+                    reader.Read();
+                    turn = reader.GetInt32();
+                }
+                else
+                {
+                    reader.Skip();
                 }
             }
         }
-    }
-}
 
-    
+        return game;
+    }
+
+}
