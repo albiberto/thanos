a5e4dfedefab19e500d353358fc63e5ff190b229
Alberto Viezzi <aviezzi@outlook.com>
2025-08-11 21:28:12 +0200

    Refactor WarArena and WarSnake initialization logic; streamline snake setup and memory handling, improve readability and maintainability

--- Thanos/War/WarField.cs
+++ Thanos/War/WarField.cs
@@ -27,35 +27,34 @@ public readonly unsafe struct WarField
         NativeMemory.Clear(memory, bitBoardsSegments * sizeof(ulong));
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void AddFood(ReadOnlySpan<Coordinate> foods)
     {
-        foreach (ref readonly var food in foods) SetBit(_foodBoard, To1D(in food));
+        foreach (ref readonly var food in foods) SetBit(_foodBoard, To1D(food));
     }
-    
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void AddHazard(ReadOnlySpan<Coordinate> hazards)
     {
-        foreach (ref readonly var hazard in hazards) SetBit(_hazardBoard, To1D(in hazard));
+        foreach (ref readonly var hazard in hazards) SetBit(_hazardBoard, To1D(hazard));
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void AddSnake(ushort* bodyPtr, int length)
-    {
-        for (var i = 0; i < length; i++) SetBit(_snakesBoard, bodyPtr[i]);
-    }
-    
+    public void SetSnakeBit(ushort position1D) => SetBit(_snakesBoard, position1D);
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static void SetBit(ulong* board, ushort position1D)
     {
         // Calcola in quale ulong dell'array si trova la nostra casella
-        var ulongIndex = position1D / 64;
+        var ulongIndex = position1D >> 6;        // divisione per 64
         
         // Calcola la posizione del bit all'interno di quell'ulong
-        var bitIndex = position1D % 64;
+        var bitIndex = position1D & 63;          // modulo 64
 
         // Imposta quel bit a 1 usando una maschera e un'operazione OR
         board[ulongIndex] |= 1UL << bitIndex;
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ushort To1D(in Coordinate coord) => (ushort)(coord.Y * _width + coord.X);
+    public ushort To1D(Coordinate coord) => (ushort)(coord.Y * _width + coord.X);
 }
