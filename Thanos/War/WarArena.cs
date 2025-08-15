6330012b87455e501193e2b543806a193dbd0fbf
Alberto Viezzi <aviezzi@outlook.com>
2025-08-15 15:49:52 +0200

    Refactor MemorySlot and WarArena; improve memory handling for WarArena initialization and streamline placement logic

--- Thanos/War/WarArena.cs
+++ Thanos/War/WarArena.cs
@@ -3,24 +3,18 @@
 namespace Thanos.War;
 
 [StructLayout(LayoutKind.Sequential)]
-public unsafe struct WarArena
+public unsafe ref struct WarArena
 {
-    private WarContext _context;
-
     private WarField* _field;
     private WarSnake* _snakes;
     
     public uint LiveSnakesCount;
     
-    public static void PlacementNew(Span<byte> arenaSpan, in WarContext context, WarSnake* snakes, WarField* field)
+    public static void PlacementNew(WarArena* arena, WarSnake* snakes, WarField* field, uint liveSnakesCount)
     {
-        // Ottiene un riferimento all'istanza di WarArena.
-        ref var arena = ref MemoryMarshal.GetReference(MemoryMarshal.Cast<byte, WarArena>(arenaSpan));
-
-        // Inizializza i campi.
-        arena._context = context;
-        arena._snakes = snakes;
-        arena._field = field;
-        arena.LiveSnakesCount = context.SnakeCount;
+        arena->_field = field;
+        arena->_snakes = snakes;
+        
+        arena->LiveSnakesCount = liveSnakesCount;
     }
 }
