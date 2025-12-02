6d9669fbbbe4aebfa652bd4b36591bc7ba1f2a90
albiberto <aviezzi@outlook.com>
2025-12-02 08:58:45 +0100

    Add unit tests for ConversionMapBuilder; verify correct population of 11x11 grid

---
+++ Thanos.Tests/Shared/ConversionMapBuilderTest.cs
@@ -0,0 +1,35 @@
+using Thanos.Shared;
+using Thanos.SourceGen;
+
+namespace Thanos.Tests;
+
+[TestFixture]
+public class ConversionMapBuilderTests
+{
+    [TestCase(7)]
+    [TestCase(11)]
+    [TestCase(19)]
+    public void Build_ShouldPopulate_Entire11x11Grid_Correctly(byte width)
+    {
+        // Arrange
+        var area = width * width;
+
+        var coordinates = new Coordinate[area];
+
+        // Act
+        ConversionMapBuilder.Build(width, coordinates);
+
+        // Assert
+        for (var i = 0; i < area; i++)
+        {
+            var expectedX = i % width;
+            var expectedY = i / width;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(coordinates[i].X, Is.EqualTo(expectedX), $"X error at index {i}");
+                Assert.That(coordinates[i].Y, Is.EqualTo(expectedY), $"Y error at index {i}");
+            });
+        }
+    }
+}
